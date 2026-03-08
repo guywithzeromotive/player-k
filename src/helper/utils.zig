3b24ebb94060618440fe9c766ad7b581928c2cb0
guywithzeromotive <ntnesi01@gmail.com>
2026-03-08 20:44:00 +0300

    clean up

--- src/helper/utils.zig
+++ src/helper/utils.zig
@@ -18,16 +18,7 @@ export fn get_tty_dimensions() callconv(.c) Window_Dimensions {
         if (win_size.ws_col > 0) {
             w.tty_width = @intCast(win_size.ws_col);
             w.tty_height = @intCast(win_size.ws_row);
-            return w;
         }
     }
-
-    // getenv returns a "C pointer" ([*c]const u8) which can be null
-    const col_ptr = c.getenv("COLUMNS");
-    const row_ptr = c.getenv("LINES");
-
-    if (col_ptr != null) w.tty_width = c.atoi(col_ptr);
-    if (row_ptr != null) w.tty_height = c.atoi(row_ptr);
-
     return w;
 }
