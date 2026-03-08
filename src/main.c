2cb37d6030c76eb36b0a4e5248fe8401f0ace9e0
guywithzeromotive <ntnesi01@gmail.com>
2026-03-08 21:10:12 +0300

    Entry point

--- src/main.c
+++ src/main.c
@@ -1,9 +1,38 @@
+#include <stdbool.h>
 #include <stdio.h>
 
 #include "../include/utils.h"
-int main() {
 
+void test();
+
+int main() {
+    int choice;
+    while (true) {
+        printf("\nEntry:\n1. About V-ANSI \n2. Video Player \n3. Test \n0. Exit\n Choice: ");
+        fflush(stdout);
+        scanf("%d", &choice);
+        switch (choice) {
+            case 1:
+                printf("Sth about me says project");
+                break;
+            case 2:
+                printf("vid player");
+                break;
+            case 3:
+                test();
+                break;
+            case 0:
+                printf("Exiting...\n\n\n");
+                fflush(stdout);
+                return 0;
+            default:
+                printf("Wrong choice. Please try again.");
+                fflush(stdout);
+        }
+    }
+}
+
+void test() {
     Window_Dimensions dimensions = get_tty_dimensions();
     printf("width : %d , height : %d\n\n" , dimensions.tty_width , dimensions.tty_height);
-    return 0;
 }
